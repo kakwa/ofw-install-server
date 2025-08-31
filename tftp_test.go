489ba9293fd591b635703511fcc1d068b2f1eaf4
kakwa <carpentier.pf@gmail.com>
2025-08-31 21:07:41 +0200

    fix tftp tests

--- tftp_test.go
+++ tftp_test.go
@@ -1,25 +1,8 @@
 package main
 
 import (
-	"net"
 	"testing"
 )
-
-func TestIPToHexString(t *testing.T) {
-	cases := []struct {
-		ip   net.IP
-		want string
-	}{
-		{net.IPv4(0, 0, 0, 0), "00000000"},
-		{net.IPv4(192, 168, 1, 10), "C0A8010A"},
-		{net.IPv4(10, 0, 0, 2), "0A000002"},
-	}
-	for _, tc := range cases {
-		if got := ipToHexString(tc.ip); got != tc.want {
-			t.Fatalf("ipToHexString(%v) = %q, want %q", tc.ip, got, tc.want)
-		}
-	}
-}
 
 func TestIsHexIPv4Name(t *testing.T) {
 	if !isHexIPv4Name("C0A8010A") {
